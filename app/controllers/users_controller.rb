f98d7f39a74b8b3b086057c912c9c2e1012bd3d7
Carlos Albenio Gutierrez Monroy <cgcarlosg@hotmail.com>
2021-01-13 18:39:46 -0500

    Add files via upload

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  before_action :current_user
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
