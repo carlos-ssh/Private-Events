52a7aeb0acc71e3a57f6c34fb73dd1165388f2de
Carlos Albenio Gutierrez Monroy <cgcarlosg@hotmail.com>
2021-01-15 14:16:46 -0500

    Delete user.rb
    fixing rubocop issue

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true
-  has_many :events, inverse_of: :creator
-  has_many :enrollments
-  has_many :attended_events, through: :enrollments, source: :event
-
-  scope :past, -> { where('date < ?', Date.today) }
-  scope :upcoming, -> { where('date >= ?', Date.today) }
-end
