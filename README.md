aeee240e0e5cfa5cb40f131dc0de2dc60cbdff9e
carlos <cgcarlosg@hotmail.com>
2021-01-14 19:36:35 -0500

    ok

--- README.md
+++ README.md
@@ -9,12 +9,15 @@ To get a local copy up and running follow these simple example steps.
 
 ## Prerequisites
 #
+
 Computer running Mac OS, Windows, or Linux Ubuntu. Internet browser: Firefox, Google Chrome, etc
 #
+
 - Install Ruby
 
 - Install Rails
 #
+
 After installing Ruby, you need to install bundler to be able to install gems and dependancies for this application.
 #
 
@@ -26,42 +29,42 @@ After installing Ruby, you need to install bundler to be able to install gems an
 - Type yarn install
 - Type rails db:migrate
 - Run with the comand: rails s to start the server.
-
 #
+
 ## Usage
 Navigate to the folder you cloned the git repository and to run our project type rails server in your terminal to start the a localhost so you can see the website. In your browser enter to localhost:3000 or the address that was output in rails server.
-
 #
+
 ## Author 1
 👤  Carlos Gutierrez
 
 - GitHub:  [@cgcarlosg](https://github.com/cgcarlosg)
 - Twitter: [@cgcarlosg1](https://twitter.com/cgcarlosg1)
 - LinkedIn: [@carlosalbeniogutierrez](www.linkedin.com/in/carlosalbeniogutierrez)
-
 #
+
 ## Author 2
 👤 Carlos Robles
 
 - GitHub: [@carlos-ssh](https://github.com/carlos-ssh)
 - Twitter: [@aom.robles](https://twitter.com/aom.robles)
 - LinkedIn: [@carlos-ssh](www.linkedin.com/in/carlos-ssh)
-
 #
+
  ## 📝 License 
 - This project is MIT licensed.
-
 #
+
 ## 🤝 Contribution
 Contributions, issues, and feature requests are welcome!
-
 #
+
 ## Acknowledgements
 - Microverse
 - The Odin Project: Active Record Queries and Associations.
 - Beter README Templates
 - Rails Guides
-
 #
+
 ## ⭐ Show your support
 Give a ⭐️ if you like this project!
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,33 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_114_173_150) do
-  create_table 'enrollments', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'event_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'status'
-    t.index ['event_id'], name: 'index_enrollments_on_event_id'
-    t.index ['user_id'], name: 'index_enrollments_on_user_id'
+ActiveRecord::Schema.define(version: 2021_01_14_173150) do
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
+    t.index ["event_id"], name: "index_enrollments_on_event_id"
+    t.index ["user_id"], name: "index_enrollments_on_user_id"
   end
 
-  create_table 'events', force: :cascade do |t|
-    t.string 'name'
-    t.date 'date'
-    t.string 'location'
-    t.string 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'user_id'
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.string "location"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'enrollments', 'events'
-  add_foreign_key 'enrollments', 'users'
+  add_foreign_key "enrollments", "events"
+  add_foreign_key "enrollments", "users"
 end
